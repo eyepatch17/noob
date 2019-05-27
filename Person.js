e6a5cdc135582bf23d8c4076f12eed8c15a281ba
eyepatch17 <50337435+eyepatch17@users.noreply.github.com>
2019-05-27 12:17:20 +0530

    Update Person.js

--- Person.js
+++ Person.js
@@ -4,6 +4,8 @@ const person = (props)=> {
   return (
     <div>
   <p onClick={props.click}>  My name {props.name} and my age is{props.age}</p>
+  <input type="text" value={props.name} onChange={props.changed} />
+<input type="number" value={props.age} onChange={props.idchanged}/>   
   
     </div>
   )
