f5e90991c8953bf5662d5796cd1ca0816bbfd484
eyepatch17 <50337435+eyepatch17@users.noreply.github.com>
2019-05-27 12:16:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,29 @@ class App extends Component {
     newperson.splice(index,1);
     this.setState({person:newperson});
   }
+  changeHandler=(event,id)=>{
+
+const personIndex=this.state.person.findIndex(p=>
+{return p.id==id});
+
+const person={...this.state.person[personIndex]};
+person.name=event.target.value;
+const persons=[...this.state.person]
+persons[personIndex]=person;
+console.log("value of"+persons);
+
+this.setState({person:persons});
+
+  }
+  idchangeHandler=(event,id)=>
+  {
+    const personIndex=this.state.person.findIndex(p=>{return p.id==id});
+    const person={...this.state.person[personIndex]};
+    person.age=event.target.value;
+    const persons=[... this.state.person];
+    persons[personIndex]=person;
+    this.setState({person:persons});
+  }
 
   render() {
 let persons=null;
@@ -34,7 +57,14 @@ persons=(
   <div>
   {this.state.person.map((person,index)=>
   {
-return <Person name={person.name} age={person.age} click={this.deleteHandler.bind(this,index)}/>
+return <Person 
+name={person.name} 
+age={person.age} 
+key={person.id} 
+click={this.deleteHandler.bind(this,index)} 
+changed={(event)=>this.changeHandler(event,person.id)} 
+idchanged={(event)=>this.idchangeHandler(event,person.id)}
+ />
   
   })}
 
